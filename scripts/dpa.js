6337a78f7362e4b0e2403dded5726c0bc7bb0e42
Brian Tully <brian.tully@acquia.com>
2020-04-30 13:16:33 -0400

    Add cache related header responses to results
    Add cache control, x-cache hit/miss, drupal cache hit/miss to page results in order to gauge cache ability of requests

--- scripts/dpa.js
+++ scripts/dpa.js
@@ -4,21 +4,33 @@ const HCCrawler = require('headless-chrome-crawler');
 const CSVExporter = require('headless-chrome-crawler/exporter/csv');
 const myURL = new URL(process.argv[2]);
 
-const FILE = '/tmp/dpa-results-' + myURL.hostname +  '.csv';
+const FILE = '/tmp/dpa-results-' + myURL.hostname + '.csv';
 
 const exporter = new CSVExporter({
   file: FILE,
-  fields: ['response.url', 'response.status', 'links.length'],
+  fields: [
+    'response.url',
+    'response.status',
+    'links.length',
+    'response.headers.cache-control',
+    'response.headers.x-cache',
+    'response.headers.x-drupal-cache'
+  ]
 });
 
-(async () => {
+(async() => {
   const crawler = await HCCrawler.launch({
     maxDepth: 5,
     exporter,
     ignoreHTTPSErrors: true,
-    allowedDomains: [myURL.hostname],
+    skipDuplicates: true,
+    allowedDomains: [myURL.hostname]
   });
-  await crawler.queue({ obeyRobotsTxt: false, url: myURL.href }, console.log("Started! " + Date()));
+  await crawler.queue({
+    obeyRobotsTxt: false,
+    skipDuplicates: true,
+    url: myURL.href
+  }, console.log("Started! " + Date()));
   await crawler.onIdle();
   await crawler.close(console.log("Yay, done! " + Date() + "\nOutput file in: " + FILE));
 })();
